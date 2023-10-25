33cdad031cb75e6c027b055b2deb86a1f13fe79d
nkired <nour.kired22@gmail.com>
2023-10-25 16:58:51 +0200

    feat(project): change my app to data matching

---
+++ src/data_matching/main.py
@@ -0,0 +1,35 @@
+import click
+
+import data_matching.__init__ as init
+from data_matching.hello_world import say_hi
+
+
+@click.group()
+def cli():
+    """OKP4 python template, program description."""
+    pass
+
+
+@cli.command
+def version():
+    """Print the application version information"""
+    click.echo(init.__version__)
+
+
+@cli.command
+@click.option(
+    "-n",
+    "--name",
+    "name",
+    type=str,
+    required=False,
+    default="friend",
+    help="A name to salute",
+)
+def main_cmd(name: str):
+    """Does something interesting..."""
+    say_hi(name)
+
+
+if __name__ == "__main__":
+    cli()
